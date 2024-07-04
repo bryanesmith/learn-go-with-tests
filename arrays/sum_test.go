c8ccb5ec2c9421ad1454568ce6c7cbfe56d65278
Bryan Smith <bryanesmith@gmail.com>
2024-07-04 09:37:12 -0400

    refactor sum_test.go

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -6,6 +6,14 @@ import (
 )
 
 func TestSum(t *testing.T) {
+
+	assertSum := func(t testing.TB, got, want int, input []int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d, want %d, input: %v", got, want, input)
+		}
+	}
+
 	t.Run("Sum an array of len 5", func(t *testing.T) {
 		input := []int{1, 2, 3, 4, 5}
 		got := Sum(input)
@@ -33,17 +41,17 @@ func TestSumAll(t *testing.T) {
 	t.Run("Sum two arrays", func(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9})
 		want := []int{3, 9}
-		assertSumAll(t, got, want)
+		assertSlicesEqual(t, got, want)
 	})
 	t.Run("Sum one arrays", func(t *testing.T) {
 		got := SumAll([]int{1, 2, 3})
 		want := []int{6}
-		assertSumAll(t, got, want)
+		assertSlicesEqual(t, got, want)
 	})
 	t.Run("Sum no arrays", func(t *testing.T) {
 		got := SumAll()
 		want := []int{}
-		assertSumAll(t, got, want)
+		assertSlicesEqual(t, got, want)
 	})
 }
 
@@ -51,30 +59,23 @@ func TestSumAllTails(t *testing.T) {
 	t.Run("Sum two arrays of len 2", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
-		assertSumAll(t, got, want)
+		assertSlicesEqual(t, got, want)
 	})
 
 	t.Run("Sum two arrays of len 3", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2, 3}, []int{0, 9, 5})
 		want := []int{5, 14}
-		assertSumAll(t, got, want)
+		assertSlicesEqual(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
-		assertSumAll(t, got, want)
+		assertSlicesEqual(t, got, want)
 	})
 }
 
-func assertSum(t testing.TB, got, want int, input []int) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %d, want %d, input: %v", got, want, input)
-	}
-}
-
-func assertSumAll(t testing.TB, got, want []int) {
+func assertSlicesEqual(t testing.TB, got, want []int) {
 	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, want %v", got, want)
